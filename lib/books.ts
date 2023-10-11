17f2ac0c392fb2278cf67c7ee942f85b75b49846
jatinkh25 <jatinkhatri25.jk@gmail.com>
2023-10-11 17:43:11 +0530

    books api changed to public

--- lib/books.ts
+++ lib/books.ts
@@ -1,9 +1,9 @@
-import { privateAPI } from '@/utils/axios'
+import { privateAPI, publicAPI } from '@/utils/axios'
 import { Book } from '@/utils/types'
 
 export const getBooks = async () => {
   try {
-    const response = await privateAPI.get('/books')
+    const response = await publicAPI.get('/books')
     return response.data.books as Book[]
   } catch (error: any) {
     throw new Error(error.response.data.error)
@@ -12,7 +12,7 @@ export const getBooks = async () => {
 
 export const getBookDetails = async (bookId: string) => {
   try {
-    const response = await privateAPI.get(`/books/${bookId}`)
+    const response = await publicAPI.get(`/books/${bookId}`)
     return response.data.book as Book
   } catch (error: any) {
     throw new Error(error.response.data.error)
